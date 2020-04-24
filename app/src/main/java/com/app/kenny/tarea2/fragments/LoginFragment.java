694c11bc90838ff7ab3ed00390934187db885dc1
Kenny Cardenas <kennyc680@gmail.com>
2020-04-24 07:39:24 -0600

    Safe args

--- app/src/main/java/com/app/kenny/tarea2/fragments/LoginFragment.java
+++ app/src/main/java/com/app/kenny/tarea2/fragments/LoginFragment.java
@@ -1,4 +1,62 @@
 package com.app.kenny.tarea2.fragments;
 
-public class LoginFragment {
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.app.kenny.tarea2.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class LoginFragment extends Fragment {
+
+    private TextInputEditText et_peso;
+    private Button btn_calc_peso;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btn_calc_peso = view.findViewById(R.id.btn_calc_peso);
+        et_peso = view.findViewById(R.id.et_peso);
+        btn_calc_peso.setEnabled(false);
+        btn_calc_peso.setOnClickListener(v -> {
+            NavDirections action = LoginFragmentDirections.actionLoginFragmentToPesoFragment(et_peso.getText().toString());
+            NavHostFragment.findNavController(LoginFragment.this).navigate(action);
+        });
+
+        et_peso.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                btn_calc_peso.setEnabled(s.toString().length() != 0);
+            }
+        });
+
+    }
+
+
 }
--- app/src/main/java/com/app/kenny/tarea2/fragments/PesoFragment.java
+++ app/src/main/java/com/app/kenny/tarea2/fragments/PesoFragment.java
@@ -1,4 +1,38 @@
 package com.app.kenny.tarea2.fragments;
 
-public class PesoFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.app.kenny.tarea2.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PesoFragment extends Fragment {
+
+    private TextView result_weight;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_peso,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        result_weight = view.findViewById(R.id.tv_result);
+        String peso = PesoFragmentArgs.fromBundle(getArguments()).getUserWeight();
+        result_weight.setText(getString(R.string.result_peso,calcMoonWeight(peso)));
+
+    }
+
+    private double calcMoonWeight(String weight){
+        double result = Double.parseDouble(weight)  * (16.5 / 100);
+        return result;
+    }
 }
